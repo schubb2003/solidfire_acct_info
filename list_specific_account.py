7ce6e5ef6908ccc60de02a2550ec0fff52848be5
schubb2003 <schubb@gmail.com>
2018-06-14 09:21:11 -0400

    var normalization
    MVIP -> src_mvip
    SFUser -> src_user
    SFUserPass -> src_pass

--- list_specific_account.py
+++ list_specific_account.py
@@ -2,7 +2,7 @@
 # Author: Scott Chubb scott.chubb@netapp.com
 # Written for Python 3.4 and above
 # No warranty is offered, use at your own risk.  While these scripts have been tested in lab situations, all use cases cannot be accounted for.
-# Usage: python list_specific_account <ID or NAME> <Account name or ID> <mvip name or IP> <user> <password>
+# Usage: python list_specific_account <ID or NAME> <Account name or ID> <src_mvip name or IP> <user> <password>
 # <ID or NAME> determine sif the user is looking for account by ID or NAME
 # <Account name or ID> this is the actual ID or name you are looking for
 
@@ -12,13 +12,13 @@ from solidfire.models import *
 
 accountType = sys.argv[1].lower()
 accountInfo = sys.argv[2]
-MVIP = sys.argv[3]
-SFUser = sys.argv[4]
-SFUserPass = sys.argv[5]
+src_mvip = sys.argv[3]
+src_user = sys.argv[4]
+src_pass = sys.argv[5]
 
 def main():
     # Use ElementFactory to get a SolidFireElement object.
-    sfe = ElementFactory.create(MVIP, SFUser, SFUserPass)
+    sfe = ElementFactory.create(src_mvip, src_user, src_pass)
     list_accounts_result = sfe.list_accounts()
 
     # If the account type was submitted as an ID process it as an integer
